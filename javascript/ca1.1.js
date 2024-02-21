db0fe3440ac1fa1c315578c13cc6bf15a0a16aa8
vanshikagupta1524 <vanshikagupta1524@gmail.com>
2024-02-21 09:05:04 +0530

    cafiles

---
+++ javascript/ca1.1.js
@@ -0,0 +1,16 @@
+// document.write(Date());
+// ===================
+// document.getElementById("demo").innerHTML="Date"+Date();
+// =======================
+// document.getElementById("image").src="flower.jpg";
+
+// ==================
+// const x=document.forms["frm1"];
+// let text="";
+// for(let i=0;i<x.length;i++){
+//     text=text+x.elements[i].value+"<br>";
+// }
+// document.getElementById("demo").innerHTML=text;
+// ================================
+// const element=document.getElementById("intro");
+// document.getElementById("demo").innerHTML="text"+element.innerHTML;
---
+++ javascript/ca1practise.js
@@ -0,0 +1,14 @@
+    // // You can also use window.onload or DOMContentLoaded event instead of this.
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     var dropdownTrigger = document.getElementById('dropdown-trigger');
+    //     var dropdownContent = document.querySelector('.dropdown-content');
+    
+    //     dropdownTrigger.addEventListener('mouseover', function() {
+    //         dropdownContent.style.display = 'block';
+    //     });
+    
+    //     dropdownTrigger.addEventListener('mouseout', function() {
+    //         dropdownContent.style.display = 'none';
+    //     });
+    // });
+    
